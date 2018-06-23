050cb3e090c22b59d870a51097bb1332c3af2df1
NguyenDinhChuong <nguyendinhchuong2607@gmail.com>
2018-06-24 00:28:42 +0700

    add product view

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var express_handlebars_sections = require('express-handlebars-sections');
 
+var homeController = require('./controllers/homeController');
 
 var app = express();
 
@@ -17,4 +18,10 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
     res.redirect('/home');
+});
+
+app.use('/home', homeController);
+
+app.listen(3000, () => {
+    console.log('Site running on port 3000');
 });
