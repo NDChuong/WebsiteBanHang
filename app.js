535b0274b673d8e429a828dda197b5bb12bfbe36
NguyenDinhChuong <nguyendinhchuong2607@gmail.com>
2018-06-24 16:08:40 +0700

    main view done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var express_handlebars_sections = require('express-handlebars-sections');
+var path = require('path');
 
 var homeController = require('./controllers/homeController');
 
