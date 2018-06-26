0fe54eeb620b8334fcc683f2dca1e2719b415128
NguyenDinhChuong <nguyendinhchuong2607@gmail.com>
2018-06-27 02:36:55 +0700

    bug in home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var product = require('../repos/productRepo');
 
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home/index');
+    // var _new = product.getNew(10);
+    // var _bestsell = product.getBestsell(10);
+    // var _bestview = product.getBestview(10);
+    var inew = [];
+    var isell = [];
+    var iview = [];
+    product.getNew(10).then(_new =>{
+        inew = _new;
+    });
+    
+    product.getBestsell(10).then(_bestsell =>{
+        isell=_bestsell;
+    });
+    product.getBestview(10).then(_bestview =>{
+        iview=_bestview;
+    })
+    var vm={
+        product_new:inew,
+        product_bestsell: isell,
+        product_bestview: iview
+    };
+    console.log(vm);
+    //console.log(_new,"====", _bestsell, "====", _bestview);
+
+    // var vm = {
+    //     product_new:_new,
+    //     product_bestsell: _bestsell,
+    //     product_bestview: _bestview
+    // }
+    res.render('home/index', vm);
 });
 
 
---
+++ repos/productRepo.js
@@ -0,0 +1,39 @@
+var db = require('../fn/db');
+var config = require('../config/config');
+
+exports.loadAll = () => {
+    var sql = 'select * from products';
+    return db.load(sql);
+}
+
+exports.loadAllByCat = (catId, offset) => {
+    var sql = `select * from products where CatID = ${catId} limit ${config.PRODUCTS_PER_PAGE} offset ${offset}`;
+    return db.load(sql);
+}
+
+exports.countByCat = catId => {
+	var sql = `select count(*) as total from products where CatID = ${catId}`;
+    return db.load(sql);
+}
+
+exports.single = proId => {
+    var sql = `select * from products where ProID = ${proId}`;
+    return db.load(sql);
+}
+
+exports.loadByBrand = (braId, offset) =>{
+    var sql = `select * from products where BraID = ${braId} limit ${config.PRODUCTS_PER_PAGE} offset ${offset}`;
+    return db.load(sql);
+}
+exports.getBestsell = (n)=>{
+    var sql = `select * from products order by Sell limit ${n}`;
+    return db.load(sql);
+}
+exports.getBestview = (n)=>{
+    var sql = `select * from products order by Clicks limit ${n}`;
+    return db.load(sql);
+}
+exports.getNew = (n)=>{
+    var sql = `select * from products order by ProID DESC limit ${n}`;
+    return db.load(sql);
+}
