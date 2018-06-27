1a7a4f60b388b6a4f8bd5b655c0db7bd748dbb4f
NDChuong <1512044@student.hcmus.edu.vn>
2018-06-27 18:42:29 +0700

    Fix view index

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,25 +7,21 @@ router.get('/', (req, res) => {
     // var _new = product.getNew(10);
     // var _bestsell = product.getBestsell(10);
     // var _bestview = product.getBestview(10);
-    var inew = [];
-    var isell = [];
-    var iview = [];
-    product.getNew(10).then(_new =>{
-        inew = _new;
-    });
-    
-    product.getBestsell(10).then(_bestsell =>{
-        isell=_bestsell;
-    });
-    product.getBestview(10).then(_bestview =>{
-        iview=_bestview;
+    var vm= {};
+    var p1 =  product.getNew(10);    
+    var p2 = product.getBestsell(10);
+    var p3 = product.getBestview(10);
+
+    Promise.all([p1,p2,p3]).then(([_new, _bestsell, _bestview])=>{
+        var vm = {
+            product_new:_new,
+            product_bestsell: _bestsell,
+            product_bestview: _bestview
+        }
+        console.log(vm);
+        res.render('home/index', vm);
     })
-    var vm={
-        product_new:inew,
-        product_bestsell: isell,
-        product_bestview: iview
-    };
-    console.log(vm);
+    
     //console.log(_new,"====", _bestsell, "====", _bestview);
 
     // var vm = {
@@ -33,7 +29,7 @@ router.get('/', (req, res) => {
     //     product_bestsell: _bestsell,
     //     product_bestview: _bestview
     // }
-    res.render('home/index', vm);
+    
 });
 
 
